71aaa767f4bff123ce1fe89924f1cb11a10c47af
thorhyno <37038116+thorhyno@users.noreply.github.com>
2018-04-06 14:55:15 +0200

    Add files via upload

--- test bac projet.py
+++ test bac projet.py
@@ -12,6 +12,9 @@ for i in range (a) :
 
 
 def placer (x):
+    tabposition = 4*[0]
+    for i in range (4) :
+        tabposition[i] = 4*[0]
     test = 0
     while test == 0 :
         hv = random.randint(0,  1)
@@ -27,7 +30,7 @@ def placer (x):
                 test = 1
                 for t in range (i, i+x):
                     tableau[t][j]=x
-            
+                    
         else : 
             i = random.randint(0,  (a-1))
             j = random.randint(0,  (b-1)-x)
@@ -39,23 +42,28 @@ def placer (x):
                 test = 1
                 for t in range (j, j+x):
                     tableau[i][t]=x
-        
-        print("test =  ",  test)  
-    print("x = ",  x)
-
+            tabposition[x-2][0] = i
+            tabposition[x-2][1] = j
+            tabposition[x-2][2] = hv
+            tabposition[x-2][3] = x
+        print("test =  ",  test)
+        print("i =  ", i ,"j =   ",   j)
+        print("x = ",  x)
     
 
 
 placer(2)
 placer(3)
+placer(3)
 placer(4)
 placer(5)
 
 for i in range (b) :
-    print(tableau[0][i],  tableau[1][i],  tableau[2][i],  tableau[3][i],   tableau[4][i],  tableau[5][i],  tableau[6][i],  tableau[7][i],  tableau[8][i],  tableau[9][i], sep="  ")
+    print(tableau[0][i],  tableau[1][i],  tableau[2][i],  tableau[3][i],   tableau[4][i],  tableau[5][i],  tableau[6][i],  tableau[7][i],  tableau[8][i],  tableau[9][i], sep=" ")
+    
+#for i in range (4) :
+    #print(tabposition[0][i], tabposition[1][i], tabposition[2][i], tabposition[3][i],  sep=" ")
 
 
-
-
-
-
+    
+    
---
+++ tototest.py
@@ -0,0 +1,13 @@
+import os    #efface la console
+os.system("cls") 
+import random
+from copy import deepcopy
+
+
+def test_sylvain(i, j) :
+    if tableau[i][j] == 0 :
+        tableau[i][j] = 6
+    elif tableau[i][j] == 2 or tableau[i][j] == 3 or tableau[i][j] == 4 or tableau[i][j] == 5 :
+        x=tableau[i][j]
+        
+    elif tableau[i][j]
